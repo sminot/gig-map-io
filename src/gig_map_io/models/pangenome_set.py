00a9019e229b0d53ad8687cf4985132a09c7cb09
Sam Minot <sminot@gmail.com>
2026-02-28 09:28:54 -0800

    PangenomeSet.rarefaction_curve()

--- src/gig_map_io/models/pangenome_set.py
+++ src/gig_map_io/models/pangenome_set.py
@@ -107,3 +107,41 @@ class PangenomeSet(DatasetDict):
 
         save_image(fig, file_prefix)
         return fig
+
+    def rarefaction_curve(
+        self,
+        col_wrap: int = 3,
+        width: int = 800,
+        height: int = 800,
+        horizontal_spacing: float = 0.05,
+        file_prefix: str | None = None
+    ) -> go.Figure:
+        """
+        Rarefaction curve of the pangenomes, faceted by pangenome.
+        """
+        fig = make_subplots(
+            rows=len(self.pangenomes) // col_wrap + 1,
+            cols=col_wrap,
+            shared_yaxes=False,
+            shared_xaxes=False,
+            horizontal_spacing=horizontal_spacing,
+            subplot_titles=[pangenome for pangenome in self.pangenomes.keys()]
+        )
+        for i, pangenome in enumerate(self.pangenomes.keys()):
+            for trace in self.pangenomes[pangenome].rarefaction_curve().data:
+                fig.add_trace(
+                    trace,
+                    row=i // col_wrap + 1,
+                    col=i % col_wrap + 1
+                )
+
+        fig.update_layout(
+            height=height,
+            width=width,
+            template="plotly_white"
+        )
+        fig.update_yaxes(
+            range=[0, None]
+        )
+        save_image(fig, file_prefix)
+        return fig
